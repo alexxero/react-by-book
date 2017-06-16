f140dd1f91bc1248adceec6d287f1a8b918a2340
alexxero <alexxero@mail.ru>
2017-06-16 12:51:30 +0300

    Readmore event handler

--- app/components/App.js
+++ app/components/App.js
@@ -3,27 +3,57 @@ var React = require('react');
 var my_news = [
     {
         author: 'user1',
-        text: 'I am the shadow, I am the light'
+        text: 'I am the shadow, I am the light',
+        fullText: 'Shinedown - Boom Lay Boom Lay Boom'
     },
     {
         author: 'user2',
-        text: 'But I am definetely not alone, Well I\'m definitely not alone'
+        text: 'But I am definetely not alone, Well I\'m definitely not alone',
+        fullText: 'Volbeat - Counting'
     },
     {
         author: 'user3',
-        text: 'I need help in many ways'
+        text: 'I need help in many ways',
+        fullText: 'Sum41 - Screaming Bloody Murder'
     }
 ]
 
 var Article = React.createClass({
+    propTypes:{
+      data: React.PropTypes.shape({
+          author: React.PropTypes.string.isRequired,
+          text: React.PropTypes.string.isRequired,
+          fullText: React.PropTypes.string.isRequired
+      })
+    },
+
+    getInitialState: function () {
+        return{
+            visible:false
+        };
+    },
+
+    readmoreClick: function (e) {
+        e.preventDefault();
+        this.setState({visible:true});
+    },
+
     render: function () {
         var author = this.props.data.author,
-            text = this.props.data.text;
+            text = this.props.data.text,
+            fullText = this.props.data.fullText,
+            visible = this.state.visible;
 
         return(
             <div className="article">
                 <p className="news__author">{author}:</p>
                 <p className="news__text">{text}</p>
+                <a href="#"
+                   className={'news__readmore ' + (visible ? 'none' : '')}
+                   onClick={this.readmoreClick}>
+                    Подробнее
+                </a>
+                <p className={'news__full-text ' + (visible ? '' : 'none')}>{fullText}</p>
             </div>
         )
     }
